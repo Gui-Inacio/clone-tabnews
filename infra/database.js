865e923e3ce860fa68b5db2fdec94027a63bdb70
Guilherme Inácio <44714868+Gui-Inacio@users.noreply.github.com>
2025-11-26 00:37:06 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl:
+      process.env.NODE_ENV === "development"
+        ? { rejectUnauthorized: false }
+        : true,
   });
   console.log("Credenciais do Postgress:", {
     host: process.env.POSTGRES_HOST,
